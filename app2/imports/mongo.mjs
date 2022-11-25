2769111144da594346e55cfee90ff407b4b30fe7
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2022-11-25 15:45:55 +0100

    fix app1 to app2

--- app2/imports/mongo.mjs
+++ app2/imports/mongo.mjs
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 const client = new MongoClient("mongodb://root:root@mongo:27017", { useNewUrlParser: true });
 const db = client.db("mongo");
 client.connect().then(() => {
-    console.log('app1 successfully connect to db.')
+    console.log('app2 successfully connect to db.')
     db.collection("users").insertOne({name: "trop fort"}).then(() => {
         db.collection("users").find({}).toArray().then((rep) => {
             console.log(rep);
